fc72a8e9c124f25069c293e1106ea87f3906637a
nguyenhoangvi99 <110900944+nguyenhoangvi99@users.noreply.github.com>
2022-10-13 15:36:34 +0700

    upload

---
+++ src/main/java/io/getarrays/userservice/filter/ObjectMapper/OjectMapper.java
@@ -0,0 +1,9 @@
+package io.getarrays.userservice.filter.ObjectMapper;
+
+import javax.servlet.ServletOutputStream;
+import java.util.Map;
+
+public class OjectMapper {
+    public void writeValue(ServletOutputStream outputStream, Map<String, String> tokens) {
+    }
+}
